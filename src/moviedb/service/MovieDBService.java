3b1e477f160330b50ea41f68973a401842ae2a98
david.omoregie <david.omoregie@its-stuttgart.de>
2022-12-15 09:15:53 +0100

    Studio and MovieDBService changes

--- src/moviedb/service/MovieDBService.java
+++ src/moviedb/service/MovieDBService.java
@@ -6,9 +6,30 @@ import moviedb.sqlService.DbAdapter;
 
 public class MovieDBService {
     private DbAdapter adapter;
-    
+
+
     private List<Movie> movies;
     private List<Person> persons;
     private List<Studio> studios;
 
+    public Movie getMoviesByID(int id)
+    {
+     return movies.stream()
+             .filter(movie -> movie.getId() == id)
+             .findAny().orElse(null);
+    }
+
+    public  Person getPersonByID(int id)
+    {
+        return persons.stream()
+                .filter(person -> person.getId() == id)
+                .findAny().orElse(null);
+    }
+
+    public Studio getStudioByID(int id)
+    {
+        return studios.stream()
+                .filter(studio -> studio.getId() == id)
+                .findAny().orElse(null);
+    }
 }
