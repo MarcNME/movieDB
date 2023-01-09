5d7c1775418e3e0348473bb3ad1b22b06d77b987
DMC\enzmamar <marc.enzmann@diconium.com>
2023-01-09 14:25:11 +0100

    Add first methods to the db adapter

--- src/moviedb/models/Movie.java
+++ src/moviedb/models/Movie.java
@@ -6,15 +6,25 @@ import java.util.List;
 import java.util.Map;
 
 public class Movie {
-    
-    private final int id;
-    private final String title;
-    private String description;
-    private String imagePath;
-    private final int studioID;
+
     private final Map<Integer, String> contributors; //Contributors to the movie <personId, role>
     private final List<Review> reviews;
-    
+    private int id;
+    private String title;
+    private String description;
+    private String imagePath;
+    private int studioID;
+
+    public Movie() {
+        this.id = -1;
+        this.title = "notChanged";
+        this.studioID = -1;
+        this.contributors = new HashMap<>();
+        this.reviews = new ArrayList<>();
+        this.description = "notChanged";
+        this.imagePath = "notChanged";
+    }
+
     public Movie(int id, String title, int studioID, Map<Integer, String> contributors) {
         this.id = id;
         this.title = title;
@@ -24,7 +34,7 @@ public class Movie {
         this.description = "";
         this.imagePath = "";
     }
-    
+
     public Movie(int id, String title, int studioID) {
         this.id = id;
         this.title = title;
@@ -34,57 +44,69 @@ public class Movie {
         this.description = "";
         this.imagePath = "";
     }
-    
+
     public int getId() {
         return id;
     }
-    
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getTitle() {
         return title;
     }
-    
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getDescription() {
         return description;
-    }
-    
-    public String getImagePath() {
-        return imagePath;
-    }
-    
-    public int getStudioID() {
-        return studioID;
-    }
-    
-    public Map<Integer, String> getContibutors() {
-        return contributors;
-    }
-    
-    public List<Review> getReviews() {
-        return reviews;
-    }
-    
-    public Review getReviewByID(int id) {
-        return reviews.stream()
-                .filter(review -> review.getId() == id)
-                .findAny().orElse(null);
     }
 
     public void setDescription(String description) {
         this.description = description;
     }
 
+    public String getImagePath() {
+        return imagePath;
+    }
+
     public void setImagePath(String imagePath) {
         this.imagePath = imagePath;
     }
-    
+
+    public int getStudioID() {
+        return studioID;
+    }
+
+    public void setStudioID(int studioID) {
+        this.studioID = studioID;
+    }
+
+    public Map<Integer, String> getContibutors() {
+        return contributors;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public Review getReviewByID(int id) {
+        return reviews.stream()
+                .filter(review -> review.getId() == id)
+                .findAny().orElse(null);
+    }
+
     public void addReview(Review review) {
         this.reviews.add(review);
     }
-    
+
     public void addContributor(int personID, String role) {
         this.contributors.put(id, role);
     }
-    
+
     public String getContributorRole(int personID) {
         return this.contributors.get(id);
     }
--- src/moviedb/sqlService/DbAdapter.java
+++ src/moviedb/sqlService/DbAdapter.java
@@ -1,5 +1,166 @@
 package moviedb.sqlService;
 
+import moviedb.models.Movie;
+import moviedb.models.Person;
+import moviedb.models.Review;
+import moviedb.models.Studio;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DbAdapter {
-    private DbConnector connector;
+    private final DbConnector connector;
+
+    public DbAdapter() {
+        this.connector = new MySqlConnector("jdbc:mysql://localhost:3306/movieDB", "root", "");
+    }
+
+    public List<Movie> getMovies() {
+        String sql = "SELECT * FROM moviedb.movie;";
+        List<Movie> movies = new ArrayList<>();
+
+        try {
+            ResultSet result = connector.executeQuery(sql);
+            while (result.next()) {
+                Movie tmp = new Movie();
+                tmp.setId(result.getInt("id"));
+                tmp.setTitle(result.getString("title"));
+                tmp.setDescription(result.getString("description"));
+                tmp.setStudioID(result.getInt("studio_id"));
+                tmp.setImagePath(result.getString("imagePath"));
+
+                movies.add(tmp);
+            }
+            result.close();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        connector.close();
+        return movies;
+    }
+
+    public Movie getMovieByID(int id) {
+        String sql = "SELECT * FROM moviedb.movie WHERE id = " + id + ";";
+        Movie movie = null;
+        try {
+            ResultSet result = connector.executeQuery(sql);
+            if (result.next()) {
+                movie = new Movie();
+                movie.setId(result.getInt("id"));
+                movie.setTitle(result.getString("title"));
+                movie.setDescription(result.getString("description"));
+                movie.setStudioID(result.getInt("studio_id"));
+                movie.setImagePath(result.getString("imagePath"));
+            }
+            result.close();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        connector.close();
+        return movie;
+    }
+
+    public void addMovie(Movie movie) {
+        String dml = String.format("INSERT INTO `movie` " +
+                        "(`title`, `description`, `studio_id`)" +
+                        " VALUES ('%s', '%s', '%d');",
+                movie.getTitle(), movie.getDescription(), movie.getStudioID());
+
+        try {
+            connector.excecuteNonQuery(dml);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public List<Person> getPeople() {
+        String sql = "SELECT * FROM moviedb.person";
+        List<Person> people = new ArrayList<>();
+        try {
+            ResultSet result = connector.executeQuery(sql);
+            while (result.next()) {
+                Person tmp = new Person(
+                        result.getInt("id"),
+                        result.getString("name"),
+                        result.getDate("birthdate"));
+
+                people.add(tmp);
+            }
+            result.close();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        connector.close();
+        return people;
+    }
+
+    public List<Review> getReviewsForMovie(int movieId) {
+        String sql = "SELECT * FROM moviedb.review WHERE movie_id = " + movieId + ";";
+        List<Review> reviews = new ArrayList<>();
+
+        try {
+            ResultSet result = connector.executeQuery(sql);
+
+            while (result.next()) {
+                Review tmp = new Review(
+                        result.getInt("id"),
+                        result.getInt("grade"),
+                        result.getString("userName"));
+
+                reviews.add(tmp);
+            }
+            result.close();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        connector.close();
+        return reviews;
+    }
+
+    public List<Studio> getStudios() {
+        String sql = "SELECT * FROM moviedb.studios;";
+        List<Studio> studios = new ArrayList<>();
+
+        try {
+            ResultSet result = connector.executeQuery(sql);
+            while (result.next()) {
+                Studio tmp = new Studio(
+                        result.getInt("id"),
+                        result.getString("name"));
+                studios.add(tmp);
+            }
+            result.close();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        connector.close();
+        return studios;
+    }
+
+    public Studio getStudioByID(int id) {
+        String sql = "SELECT * FROM moviedb.studio WHERE id = " + id + ";";
+        Studio studio = null;
+        try {
+            ResultSet result = connector.executeQuery(sql);
+
+            if (result.next()) {
+                studio = new Studio(
+                        result.getInt("id"),
+                        result.getString("name"));
+            }
+            result.close();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        connector.close();
+        return studio;
+    }
 }
