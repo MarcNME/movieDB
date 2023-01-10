4a809375331293c00cbaa4e7586c5160f9e4423b
DMC\enzmamar <marc.enzmann@diconium.com>
2023-01-10 08:58:26 +0100

    Add methods for SQL queries

--- src/moviedb/models/Studio.java
+++ src/moviedb/models/Studio.java
@@ -1,18 +1,20 @@
 package moviedb.models;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Studio {
 
     private final int id;
     private final String name;
-    List studios = new ArrayList<>();
-    public Studio(int id, String name)
-    {
+
+    public Studio(int id, String name) {
         this.id = id;
         this.name = name;
     }
-    public int getId() {return id;}
 
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
