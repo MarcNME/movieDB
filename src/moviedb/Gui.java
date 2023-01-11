250d1ce1231086a454339ecfc0d3ffbac8f8cd48
Marc Enzmann <marc.enzmann@satis-faction.de>
2023-01-11 09:00:50 +0100

    Load data in the Gui

--- src/moviedb/Gui.java
+++ src/moviedb/Gui.java
@@ -8,6 +8,10 @@ public class Gui extends javax.swing.JFrame {
 
     public Gui() {
         initComponents();
+        service = new MovieDBService();
+
+        service.refresh();
+        System.out.println("There are " + service.getMovies().size() + " Movies in the database");
     }
 
     @SuppressWarnings("unchecked")
@@ -73,7 +77,7 @@ public class Gui extends javax.swing.JFrame {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
@@ -93,7 +97,6 @@ public class Gui extends javax.swing.JFrame {
         }
         //</editor-fold>
         //</editor-fold>
-
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
